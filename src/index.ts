4f9465d34408c74573b04f6928fbc91b9c403dc0
dbudaiya <2645472474@qq.com>
2022-09-24 09:57:12 +0800

    feat: 补充配置文件

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
 export function add(...args: number[]) {
   return args.reduce((a, b) => a + b, 0)
 }
+
+export const result = 'footer'
+
+export function timeout() {
+  return 'this is data'
+}
+
+declare const decLiteral: number;
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,14 +1,7 @@
 import { describe, expect, it } from 'vitest'
-import { add } from '../src/index'
 
 describe('should', () => {
-  it('exported', () => {
-    expect(1).toEqual(1)
-  })
-})
-
-describe('test function', () => {
-  it.concurrent('add', () => {
-    expect(add(1, 2, 3)).toEqual(6)
+  it('exported', (ctx) => {
+    console.log('ctx', ctx)
   })
 })
